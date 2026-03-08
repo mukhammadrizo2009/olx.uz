407b96253102de347b31258073dab2213384c772
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-03-08 18:04:08 +0500

    Urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import TelegramLoginView, RefreshTokenView, LogoutView, MeView, UpgradeToSellerView, SellerProductsView, SellerDetailView
 
 urlpatterns = [
+    
     path('v1/auth/telegram-login/', TelegramLoginView.as_view(), name="telegram-login"),
     path('v1/auth/refresh/', RefreshTokenView.as_view(), name='refresh'),
     path('v1/auth/logout/', LogoutView.as_view(), name='logout-user'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('v1/users/me/upgrade-to-seller/', UpgradeToSellerView.as_view(), name='upgrade-seller'),
     path('v1/sellers/<int:pk>/', SellerDetailView.as_view(), name='seller-detail'),
     path('v1/sellers/<int:user_id>/products/', SellerProductsView.as_view(), name='seller-products'),
+    
 ]
