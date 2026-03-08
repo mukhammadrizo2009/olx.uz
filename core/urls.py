00a0f3bbe5ead0634bfb791ae53bfe9a7a6dff79
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-03-08 18:01:03 +0500

    Core Urls

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('api/', include("apps.users.urls")),
     path('api/', include("apps.categories.urls")),
@@ -31,4 +32,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
 ]
