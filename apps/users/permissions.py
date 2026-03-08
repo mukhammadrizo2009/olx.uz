6fc64b3ca393b5afae9bb4d160ce76f4cdc12da7
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-03-08 18:05:12 +0500

    Permissions

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+
 class IsCustomer(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "customer"
